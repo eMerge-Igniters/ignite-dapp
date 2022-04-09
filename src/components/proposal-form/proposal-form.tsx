c05ed5bdb93c1099d595c04ef0926c4468990b54
AndreBa <Andre_Barrientos@ultimatesoftware.com>
2022-04-09 15:53:26 -0400

    update task-drawer

--- src/components/proposal-form/proposal-form.tsx
+++ src/components/proposal-form/proposal-form.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Listen, State } from '@stencil/core';
 
 @Component({
   tag: 'proposal-form',
@@ -18,6 +18,22 @@ export class ProposalForm {
   votingMonInput;
   tagInput;
 
+  @State() titleInputDisabled: boolean = true;
+  @State() descriptionInputDisabled: boolean = true;
+  @State() categoryInputDisabled: boolean = true;
+
+  @Listen('ukgInput')
+  @Listen('ukgChange')
+  handleInput(ev) {
+    if (ev.target === this.titleInput) {
+      this.titleInputDisabled = ev.target.value ? false : true;
+    } else if (ev.target === this.descriptionInput) {
+      this.descriptionInputDisabled = ev.target.value ? false : true;
+    } else if (ev.target === this.categoryInput) {
+      this.categoryInputDisabled = ev.target.value ? false : true;
+    }
+  }
+
   renderTags() {
     return this.tagValues.map(tag => (
       <ukg-chip value={tag} onClick={ev => this.toggleChip(ev)}>
@@ -36,20 +52,34 @@ export class ProposalForm {
   }
 
   async clearProposalForm() {
+    this.titleInput.value = null;
+    this.descriptionInput.value = null;
+    this.categoryInput.value = null;
+    this.votingMonInput.value = null;
+    const chips = this.tagInput.querySelectorAll('ukg-chip');
+    chips.forEach(chip => (chip.selected = false));
     this.stepperEl.clear();
   }
 
-  getSelectedChips() {
+  getSelectedTags() {
     const chips = this.tagInput.querySelectorAll('ukg-chip');
     return [...chips].filter(chip => chip.selected).map(chip => chip.value);
   }
 
   getValues() {
-    console.log(this.titleInput.value);
-    console.log(this.descriptionInput.value);
-    console.log(this.categoryInput.value);
-    console.log(this.votingMonInput.value);
-    console.log(this.getSelectedChips());
+    const title = this.titleInput.value;
+    const description = this.descriptionInput.value;
+    const category = this.categoryInput.value;
+    const votingMonth = this.votingMonInput.value;
+    const tags = this.getSelectedTags().join(',');
+    const values = {
+      title: title,
+      description: description,
+      category: category,
+      votingMonth: votingMonth,
+      tags: tags,
+    };
+    return JSON.stringify(values);
   }
 
   render() {
@@ -57,12 +87,12 @@ export class ProposalForm {
       <Host>
         <ukg-stepper ref={el => (this.stepperEl = el)}>
           <ukg-step is-active step="1">
-            Proposal Title
+            Title
             <form slot="content">
               <ukg-input-container>
-                <ukg-input required ref={el => (this.titleInput = el)}></ukg-input>
+                <ukg-input ref={el => (this.titleInput = el)}></ukg-input>
               </ukg-input-container>
-              <ukg-button emphasis="mid" ukg-stepper-next-button>
+              <ukg-button emphasis="mid" ukg-stepper-next-button disabled={this.titleInputDisabled}>
                 Continue
               </ukg-button>
             </form>
@@ -71,9 +101,9 @@ export class ProposalForm {
             Description
             <form slot="content">
               <ukg-input-container size="maximum">
-                <ukg-textarea required ref={el => (this.descriptionInput = el)}></ukg-textarea>
+                <ukg-textarea ref={el => (this.descriptionInput = el)}></ukg-textarea>
               </ukg-input-container>
-              <ukg-button emphasis="mid" ukg-stepper-next-button>
+              <ukg-button emphasis="mid" ukg-stepper-next-button disabled={this.descriptionInputDisabled}>
                 Continue
               </ukg-button>
             </form>
@@ -86,7 +116,7 @@ export class ProposalForm {
                   {this.renderSelectOptions(this.categories)}
                 </ukg-select>
               </ukg-input-container>
-              <ukg-button emphasis="mid" ukg-stepper-next-button>
+              <ukg-button emphasis="mid" ukg-stepper-next-button disabled={this.categoryInputDisabled}>
                 Continue
               </ukg-button>
             </form>
@@ -110,16 +140,14 @@ export class ProposalForm {
               <ukg-chip-group align="left" error="false" error-label="Error" fitted-type="none" aria-label="Tags" ref={el => (this.tagInput = el)}>
                 {this.renderTags()}
               </ukg-chip-group>
-              <ukg-button-group right-align>
-                <ukg-button emphasis="mid" onClick={() => this.getValues()}>
-                  Submit
-                </ukg-button>
-              </ukg-button-group>
+              <ukg-button emphasis="mid" onClick={() => this.getValues()}>
+                Submit
+              </ukg-button>
             </form>
           </ukg-step>
         </ukg-stepper>
         <ukg-button-group right-align>
-          <ukg-button emphasis="mid" onClick={() => this.stepperEl.clear()}>
+          <ukg-button emphasis="mid" onClick={() => this.clearProposalForm()}>
             Clear
           </ukg-button>
         </ukg-button-group>
