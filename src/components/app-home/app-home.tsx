bb8c145f51bedd86fa16d126daec5337a2f33e3d
rleal1995@hotmail.com <rleal1995@hotmail.com>
2022-04-09 10:16:52 -0400

    clean up

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -2,8 +2,8 @@ import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'app-home',
-  styleUrl: 'app-home.css',
-  shadow: true,
+  styleUrl: 'app-home.scss',
+  scoped: true,
 })
 export class AppHome {
   render() {
--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -2,22 +2,24 @@ import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'app-root',
-  styleUrl: 'app-root.css',
-  shadow: true,
+  styleUrl: 'app-root.scss',
+  scoped: true,
 })
 export class AppRoot {
   render() {
     return (
       <div>
-        <header>
-          <h1>Stencil App Starter</h1>
-        </header>
-
+        <ukg-nav-header heading="Community proposals">
+          <div slot="icons">
+            <ukg-button icon-only>
+              <i slot="icon-only" class="fa-solid fa-large"></i>
+            </ukg-button>
+          </div>
+        </ukg-nav-header>
         <main>
           <stencil-router>
             <stencil-route-switch scrollTopOffset={0}>
               <stencil-route url="/" component="app-home" exact={true} />
-              <stencil-route url="/profile/:name" component="app-profile" />
             </stencil-route-switch>
           </stencil-router>
         </main>
